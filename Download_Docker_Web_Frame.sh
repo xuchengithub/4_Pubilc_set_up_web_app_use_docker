815826a36b0e4c77651bbeeccca9077b525ba1c7
xu chen <xuchen1522@gmail.com>
2021-05-31 17:40:39 +0900

    Update Download_Docker_Web_Frame.sh

--- Download_Docker_Web_Frame.sh
+++ Download_Docker_Web_Frame.sh
@@ -1,3 +1,8 @@
+echo "Setting envivonment variables for the webcam" 
+xhost +local:docker
+# XSOCK=/tmp/.X11-unix
+# XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge -
 
 #-e filename 如果 filename存在，则为真
 DIR="./Pubilic_XUCHEN_web_app_frame1"
@@ -12,3 +17,5 @@ fi
 
 rm -rf .git
 # docker-compose up -d
+
+
